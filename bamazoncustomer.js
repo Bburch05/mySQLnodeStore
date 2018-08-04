1174ed952ef1b275702ed01c530d96d71233aabb
Brian Burch <Bburch05@gmail.com>
2018-08-04 13:49:27 -0400

    finished customer page

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,55 +1,102 @@
 var inquirer = require("inquirer");
-var table = require("table");
+const {table} = require('table')
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
     user: "root",
-  
-    // Your password
     password: "root",
     database: "bamazon_db"
   });
   
+var tableData = [["Item Id","Product","Department","# Left", "Price ($)"]]
+var buyID
 
-function bamazonMenu(){
+function displayTable(){
     connection.query("SELECT * FROM products",function(err,res){
         if (err) throw err;
+
     for (var i = 0; i < res.length; i++){
-        console.log(`${res[i].id} --|-- ${res[i].item} --|-- ${res[i].qty}`)
+        var newArray = [res[i].id,res[i].item,res[i].department,res[i].qty,res[i].price];
+        tableData.push(newArray);
     }
-    connection.end();
-    })
-    
+    console.log("\n"+table(tableData));
+
+    });
+};
+
+function bamazonMenu(){
     // running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-    // inquirer.prompt([
-    //     {
-    //         name: buyId,
-    //         message:"Please enter the ID of the item that you would like to buy.",
-    //         type: "input",
-    //         validate : function(input){
-    //             return parseInt(input) !== NaN
-    //         }
-    //     },
-    //     {
-    //         name: qnty,
-    //         message: "How much of that item would you like to buy?",
-    //         type: "input",
-    //         validate: function(input){
-    //             return parseInt(input) < 
-    //         }
-    //     }
-    // ]).then(function(user){
+     // The first should ask them the ID of the product they would like to buy.  
+    inquirer.prompt([
+        {
+            name: "buyId",
+            message:"Please enter the ID of the item that you would like to buy.",
+            type: "input",
+            validate : function(input){
+                if (!isNaN(parseInt(input))){
+                    return true
+                }
+                console.log("\nPlease enter a valid number")
+            }
+        }
+    ]).then(function(user){
+        buyID = user.buyId
+        buyItem();
+    });
 
-    // });
-    // The first should ask them the ID of the product they would like to buy.
+    };
+    
+    
+       // The second message should ask how many units of the product they would like to buy.
+    function buyItem(){
+        connection.query("SELECT * FROM products WHERE id = ?",[buyID],function(err,res){
+            if (err) throw err
 
-    // The second message should ask how many units of the product they would like to buy.
+            var item = res[0].item
+
+            inquirer.prompt(
+                {
+                    name: "qty",
+                    type: "input",
+                    message: `How much of ${res[0].item} would you like to buy?`,
+                    validate: function(input){
+                        if(!isNaN(parseInt(input)) && parseInt(input) <= parseInt(res[0].qty)){
+                            return true;
+                        }
+                        else if (isNaN(parseInt(input))){
+                            console.log("\nPlease enter a number.");
+                        }
+                        else if (parseInt(input) > parseInt(res[0].qty)){
+                            console.log("\nThat is more than we have!")
+                        }
+                    }
+                }
+        ).then(function(user){
+            var buyAmount = parseInt(user.qty);
+            var price = parseFloat(res[0].price)
+            var newTotal = parseInt(res[0].qty) - buyAmount;
+            connection.query("UPDATE products SET ? WHERE ?",
+            [
+              {
+                qty: newTotal
+              },
+              {
+                id: buyID
+              }
+            ],function(err,res){
+                if (err) {
+                    console.log(err)
+                }
+                console.log(`You have sucessfully bought $${price * buyAmount} worth of ${item} `)
+                connection.end();
+            })  
+        })
+        });
+    
+    };
+
     
     // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
     
@@ -58,10 +105,7 @@ function bamazonMenu(){
     // if your store _does_ have enough of the product, you should fulfill the customer's order.
     // Once the update goes through, show the customer the total cost of their purchase.
     
-};
 
-function buyItem(){
 
-};
-
+displayTable();
 bamazonMenu();
